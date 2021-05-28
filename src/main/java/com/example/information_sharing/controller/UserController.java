f3a1e629916952234491441bf5ede4571aaf6ce1
leafstar25 <1290936744@qq.com>
2021-05-28 16:18:18 +0800

    Merge remote-tracking branch 'origin/master'

---
+++ src/main/java/com/example/information_sharing/controller/UserController.java
@@ -0,0 +1,43 @@
+package com.example.information_sharing.controller;
+
+import com.example.information_sharing.VO.ResultVO;
+import com.example.information_sharing.form.LoginForm;
+import com.example.information_sharing.form.RegisterForm;
+import com.example.information_sharing.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author hgp
+ * @version 1.0
+ * @date 2021/5/18 13:44
+ */
+@RestController
+@CrossOrigin
+@Slf4j
+@RequestMapping("/user")
+@Api(tags = "用户接口")
+public class UserController {
+	@Autowired
+	private UserService userService;
+
+	@PostMapping("/login")
+	@ApiOperation("登录")
+	public ResultVO login(LoginForm loginForm, HttpServletResponse response) {
+		return userService.login(loginForm, response);
+	}
+
+	@PostMapping("/register")
+	@ApiOperation("注册")
+	public ResultVO register(RegisterForm form) {
+		return userService.register(form);
+	}
+}
